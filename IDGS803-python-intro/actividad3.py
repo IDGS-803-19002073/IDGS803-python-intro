3e5fad7ae6daa022a75a643c39f27c5a2ed98bfb
Maria Contreras <19002073@utleon.edu.mx>
2023-01-27 18:35:58 -0600

    intro practica

---
+++ IDGS803-python-intro/actividad3.py
@@ -0,0 +1,64 @@
+class operasbas():
+    #definir propiedades de clase
+    num1=0
+    num2=0
+    res=0
+    
+    #definir constructor de la clase   
+    def sumar(self,a,b):
+        self.num1=a
+        self.num2=b
+        self.res=self.num1+self.num2
+        return self.res
+
+    def restar(self,a,b):
+        self.num1=a
+        self.num2=b
+        self.res=self.num1-self.num2
+        return self.res
+
+    def multiplicar(self,a,b):
+        self.num1=a
+        self.num2=b
+        self.res=self.num1*self.num2
+        return self.res
+
+    def dividir(self,a,b):
+        self.num1=a
+        self.num2=b
+        self.res=self.num1/self.num2
+        return self.res
+
+def main():
+    obj=operasbas()
+    print("Menu OPCION sumar=1 , restar=2 , multiplicar=3 , dividir=4 , salir=5")
+    num1=int(input("Escoger: "))  
+    while num1!=5:
+      if num1 == 1:
+            numero1=int(input("dame numero 1: "))
+            numero2=int(input("dame numero 2: "))
+            return print("{} + {} = {}".format(numero1, numero2, obj.sumar(numero1,numero2)))
+            
+      elif num1 == 2:
+            numero1=int(input("dame numero 1: "))
+            numero2=int(input("dame numero 2: "))
+            return print("{} - {} = {}".format(numero1, numero2, obj.restar(numero1,numero2)))
+            
+      elif num1 == 3:
+            numero1=int(input("dame numero 1: "))
+            numero2=int(input("dame numero 2: "))
+            return print("{} * {} = {}".format(numero1, numero2, obj.multiplicar(numero1,numero2)))
+            
+      elif num1 == 4:
+            numero1=int(input("dame numero 1: "))
+            numero2=int(input("dame numero 2: "))
+            return print("{} / {} = {}".format(numero1, numero2, obj.dividir(numero1,numero2)))
+            
+        
+      elif num1 == 5:
+            return "adios"
+            
+
+if __name__=='__main__':
+    main()
+        
---
+++ IDGS803-python-intro/tema16-clases.py
@@ -0,0 +1,16 @@
+class padre():
+    x=1
+
+class operasbas(padre):
+    #definir propiedades de clase
+    num1=0
+    num2=0
+    res=0
+    #definir constructor de la clase
+    def suma(self,a,b) :
+        x=1
+        self.num1=a
+        self.num2=b
+        return a+b
+        
+    #defnimos los metodos de la clase
